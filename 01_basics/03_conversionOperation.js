bd67a1978489e82d25eaf7a5fb7c438e5984d26b
Adarsh Singh <adarshsinghhlm2004@gmail.com>
2026-07-11 02:07:31 +0530

    javaScript operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,15 +26,45 @@ false - 0
 
 */
 
-let isLogged = 1
-let isLogged1 = 0
+// let isLogged = 1
+// let isLogged1 = 0
 
-let isLogged2= ""
-let isLogged3 = "askjs"
+// let isLogged2= ""
+// let isLogged3 = "askjs"
 
-console.log(Boolean(isLogged))  // true
-console.log(Boolean(isLogged1)) // false
-console.log(typeof String(isLogged))  // string
-console.log( typeof String(isLogged1)) // string
-console.log(Boolean(isLogged2)) // false
-console.log(Boolean(isLogged3)) // true
+// console.log(Boolean(isLogged))  // true
+// console.log(Boolean(isLogged1)) // false
+// console.log(typeof String(isLogged))  // string
+// console.log( typeof String(isLogged1)) // string
+// console.log(Boolean(isLogged2)) // false
+// console.log(Boolean(isLogged3)) // true
+
+/*********************************Operation **********************************************/ 
+
+let value = 3
+let negValue = -value
+
+let str1 = "1"
+let str2 = "2"
+let str3 = 2
+console.log(str1 + str2) //12
+console.log(str1+str3) //12
+console.log(str3+ str1) //21
+
+console.log("1"+2+2) //122
+console.log(2+"1"+2) // 212
+console.log(2+2+"1") // 41
+
+console.log(+true)  //1
+
+let count = 1;
+console.log(++count) // prefix 2
+console.log(count) // 2
+let count2 = 1
+console.log(count2++) // postfix   1
+console.log(count2) // 2
+
+
+
+
+
