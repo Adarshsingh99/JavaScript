b95ef695e0a429091c07e1d0ea96ef5d15544734
Adarsh Singh <adarshsinghhlm2004@gmail.com>
2026-07-11 02:51:14 +0530

    dataType summery

---
+++ 01_basics/DataType-Summry.js
@@ -0,0 +1,27 @@
+// kis tarah se data ko memory me rakha jata hai acess kiya jata hai iske 2 types hai
+// 1. primitive
+/*
+      7 types - String,Number,Boolean, null , Undefined,Symbol,BigInt
+    
+      const val1 = symbol('123')
+      const val2 = symbol('123')
+
+      val1===val2  // give false result
+
+*/
+
+// 2. non-primitive - Array, Objects, Function
+
+const heros = ["ironman", "superman","thor"]
+ console.log(typeof(heros))  // object
+
+let obj = {
+    name : "adarsh",
+    age : 22,
+}
+console.log(typeof(obj)) // object
+
+myfuction=function  (){
+
+}
+console.log(typeof(myfuction)) // object-function
