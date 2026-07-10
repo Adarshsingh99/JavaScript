a5b1ce2408f1fae58036ccb1760da99190e496ac
Adarsh Singh <adarshsinghhlm2004@gmail.com>
2026-07-11 01:27:21 +0530

    learn about dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version
+// but today browser are advance they use automatically this mode
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(
+    "hello"
+) // code should be readibility
+
+let name = "adarsh"  // string : in double quotes
+let age = 18  // number : 2 to power 53
+let isLogged = false  // boolean : true or false
+let value = null // standlone value
+let value; // undefined : i declare but does not give value  
+// symbol :-uniqueness find krne k liye bahut jyada use hota hai
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
